24b9b0e5dfac7b1b006f3ce3ae9863326a87c9ef
kulwinder2510 <kulwinder9053@gmail.com>
2023-02-28 11:09:04 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        System.out.println(Student.school);
+//    }
+//}
+
+
+//        Student s1 = new Student();
+//        s1.name = "tom";
+//        s1.age = 34;
+//
+//        Student s2 = new Student();
+//        s2.name = "pam";
+//        s2.age = 29;
+//    }
+//}
+
+
+        Movie m1 = new Movie("herry potter1", 10);
+        Movie m2 = new Movie("herry potter2", 10);
+        Movie m3 = new Movie("herry potter3", 10);
+        Movie m4 = new Movie("herry potter4", 10);
+        Movie m5 = new Movie("herry potter5", 10);
+        System.out.println(Movie.TicketCounter);
+    }
+}
---
+++ src/Movie.java
@@ -0,0 +1,10 @@
+public class Movie {
+    String name;
+    int price;
+    static int TicketCounter = 0;
+    public Movie(String name,int price){
+        this.name = name;
+        this.price = price;
+        TicketCounter++;
+    }
+}
